cdaa4863e1814686fb7b393249ecdcd2a03b61e0
wupeng <591726269@qq.com>
2019-11-04 16:38:42 +0800

    second commit

---
+++ src/main/java/com/wzp/lineloss/controller/indexController.java
@@ -0,0 +1,16 @@
+package com.wzp.lineloss.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author wzp
+ * @date 2019-11-04 11:38
+ */
+@Controller
+public class indexController {
+    @GetMapping("/")
+    public String index(){
+        return "login";
+    }
+}
