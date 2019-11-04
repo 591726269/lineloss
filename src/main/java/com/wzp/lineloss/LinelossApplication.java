a03b13692a6dd1305a66814e47e509116e43768d
wupeng <591726269@qq.com>
2019-11-04 10:47:38 +0800

    first commit

---
+++ src/main/java/com/wzp/lineloss/LinelossApplication.java
@@ -0,0 +1,13 @@
+package com.wzp.lineloss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinelossApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinelossApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wzp/lineloss/LinelossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzp.lineloss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinelossApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
